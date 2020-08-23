2b14e6b6ceceb09a6a0dcee94f693183e6f2b92f
wk <21>
2020-08-23 22:33:55 +0800

    增加数据库连接池连接方式

--- src/main/java/com/company/sqlsession/TestAnnotation.java
+++ src/main/java/com/company/sqlsession/TestAnnotation.java
@@ -1,11 +1,13 @@
 package com.company.sqlsession;
 
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.company.mapper.OrderMapper;
 import com.company.mapper.UserMapper;
 import com.company.po.Order;
 import com.company.po.User;
 import com.company.utils.InitLogRecord;
+import com.sun.xml.internal.bind.v2.util.DataSourceSource;
 import com.sun.xml.internal.ws.api.config.management.policy.ManagementAssertion;
 import org.apache.ibatis.datasource.pooled.PooledDataSourceFactory;
 import org.apache.ibatis.logging.log4j.Log4jImpl;
@@ -30,16 +32,25 @@ import java.util.Properties;
 public class TestAnnotation {
     public static void main(String[] args) {
        // InitLogRecord.initLog();
-        Properties properties = new Properties();
+        /*
+        * 第一种创建数据库链接
+        * */
+//        Properties properties = new Properties();
+//        properties.setProperty("driver","com.mysql.jdbc.Driver");
+//        properties.setProperty("url","jdbc:mysql://127.0.0.1:3306/mybatis?useSSL=false&useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai");
+//        properties.setProperty("username","root");
+//        properties.setProperty("password","root");
+//        PooledDataSourceFactory pooledDataSourceFactory = new PooledDataSourceFactory();
+//
+//        pooledDataSourceFactory.setProperties(properties);
+//        DataSource dataSource = pooledDataSourceFactory.getDataSource();
 
-        properties.setProperty("driver","com.mysql.jdbc.Driver");
-        properties.setProperty("url","jdbc:mysql://127.0.0.1:3306/mybatis?useSSL=false&useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai");
-        properties.setProperty("username","root");
-        properties.setProperty("password","root");
-        PooledDataSourceFactory pooledDataSourceFactory = new PooledDataSourceFactory();
-
-        pooledDataSourceFactory.setProperties(properties);
-        DataSource dataSource = pooledDataSourceFactory.getDataSource();
+/*
+*
+* 第二种通过druid连接池
+*
+* */
+        DataSource dataSource = getDataSource();
         TransactionFactory transactionFactory = new JdbcTransactionFactory();
 
 
@@ -60,5 +71,13 @@ public class TestAnnotation {
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+    static DataSource getDataSource(){
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUrl("jdbc:mysql://127.0.0.1:3306/mybatis?useSSL=false&useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai");
+        druidDataSource.setUsername("root");
+        druidDataSource.setPassword("root");
+        druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return druidDataSource;
     }
 }
